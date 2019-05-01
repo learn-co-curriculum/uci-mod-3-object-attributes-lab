1cb18d4d483cd156fbb00e7d6dc39e857130019e
maxwellbenton <maxwellbenton@gmail.com>
2019-05-01 17:18:02 -0400

    adds solution

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Add your code here
+class Dog
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
-# Add your code here
+class Person
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+end
